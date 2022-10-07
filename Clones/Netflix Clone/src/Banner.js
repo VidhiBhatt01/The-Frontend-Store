b2544cf6c6e2853004f4541a246d66663ce6fe24
psb27 <singhprateek2704@gmail.com>
2022-10-07 22:27:44 +0530

    Added Netflix Clone to the Clone Directory

--- Clones/Netflix Clone/src/Banner.js
+++ Clones/Netflix Clone/src/Banner.js
@@ -21,7 +21,7 @@ const Banner = () => {
     fetchData();
   }, []);
 
-  console.log(movie);
+  //console.log(movie);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + ('...') : string;
