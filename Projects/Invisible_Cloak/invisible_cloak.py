4a56b55aeaf9701385a237bb2ead6f431bc27409
Robinrai2612 <robinsrai2000@gmail.com>
2023-10-22 12:15:23 +0530

    Added Images and soe minor chnages

--- Projects/Invisible_Cloak/invisible_cloak.py
+++ Projects/Invisible_Cloak/invisible_cloak.py
@@ -1,49 +1,70 @@
 import cv2
 import numpy as np
 
-# Initialize the webcam or capture video
-cap = cv2.VideoCapture(0)
+def detect_color(frame):
+  # Convert the frame to HSV color space.
+  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# Load the background image
-background = cv2.imread('Images/pic2.jpg')
+  # Threshold the HSV image to detect the red color.
+  lower_red = np.array([0, 120, 0])
+  upper_red = np.array([10, 255, 100])
+  mask = cv2.threshold(hsv, lower_red, upper_red, cv2.THRESH_BINARY)[1]
 
-while cap.isOpened():
+  return mask
+
+def apply_mask(frame, mask):
+  # Apply the mask to the frame.
+  masked_frame = cv2.bitwise_and(frame, frame, mask=mask)
+
+  return masked_frame
+
+def combine_frames(frame1, frame2):
+  # Combine the two frames.
+  combined_frame = cv2.addWeighted(frame1, 0.5, frame2, 0.5, 0)
+
+  return combined_frame
+
+def remove_noise(mask):
+  # Remove noise from the mask.
+  kernel = np.ones((5, 5), np.uint8)
+  eroded_mask = cv2.erode(mask, kernel, iterations=1)
+  dilated_mask = cv2.dilate(eroded_mask, kernel, iterations=1)
+
+  return dilated_mask
+
+def main():
+  # Initialize the camera.
+  cap = cv2.VideoCapture(0)
+
+  # Store a single frame before starting the infinite loop.
+  ret, frame = cap.read()
+  background = frame
+
+  # Create a mask for the red color.
+  mask = detect_color(frame)
+
+  while True:
+    # Capture a frame from the camera.
     ret, frame = cap.read()
-    if not ret:
-        break
 
-    # Convert the frame to HSV color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Apply the mask to the frame.
+    masked_frame = apply_mask(frame, mask)
 
-    # Define the range of color for the cloak (green color in this example)
-    lower_green = np.array([40, 40, 40])
-    upper_green = np.array([80, 255, 255])
+    # Combine the masked frame with the background.
+    combined_frame = combine_frames(masked_frame, background)
 
-    # Create a mask for the cloak color
-    mask = cv2.inRange(hsv, lower_green, upper_green)
+    # Remove noise from the mask.
+    eroded_mask = remove_noise(mask)
 
-    # Perform morphological operations to clean the mask
-    kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    # Display the frame.
+    cv2.imshow('red cloak', combined_frame)
 
-    # Invert the mask
-    mask_inv = cv2.bitwise_not(mask)
+    # Check if the user wants to quit.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
 
-    # Extract the cloak from the frame
-    cloak = cv2.bitwise_and(frame, frame, mask=mask_inv)
+  # Release the camera.
+  cap.release()
 
-    # Extract the background region from the background image
-    background_region = cv2.bitwise_and(background, background, mask=mask)
-
-    # Combine the cloak and background region
-    result = cv2.add(cloak, background_region)
-
-    # Display the result
-    cv2.imshow("Invisible Cloak", result)
-
-    if cv2.waitKey(1) == 27:  # Press 'Esc' to exit
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+  main()
